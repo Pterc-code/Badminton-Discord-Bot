e2b803fada680298911d90fc63849b48c711be34
William Gao <william2010.gao@gmail.com>
2024-07-09 19:36:28 -0400

    fixed

--- cogs/CourtBookingFiles/BookCourt.py
+++ cogs/CourtBookingFiles/BookCourt.py
@@ -34,26 +34,29 @@ from datetime import datetime
 def update_application_cookie():
     print("updating cookie")
     global application_cookie
-    x = requests.get(
-        'https://recreation.utoronto.ca/booking/33215bab-05b9-41de-be04-c9ae496d5609/slots/4c99c8bd-f117-4603-bba6-c8e2e9614799/2024/%s' % booking_date,
-        headers={"accept": "*/*",
-                 "accept-encoding": "gzip, deflate, br",
-                 "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                 "referer": "https://recreation.utoronto.ca/booking/af97abfd-f094-49c4-8a17-9330879ed6cc",
-                 "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                 "sec-ch-ua-mobile": "?0",
-                 "sec-ch-ua-platform": "'windows'",
-                 "sec-fetch-dest": "empty",
-                 "sec-fetch-mode": "cors",
-                 "sec-fetch-site": "same-origin",
-                 "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-                 "x-requested-with": "XMLHttpRequest",
-                 "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
-                 })
-    updated_cookie = x.cookies.get_dict().get('.AspNet.ApplicationCookie')
-    print(updated_cookie)
-    application_cookie = application_cookie if updated_cookie is None else updated_cookie
 
+    try:
+        x = requests.get(
+            'https://recreation.utoronto.ca/booking/33215bab-05b9-41de-be04-c9ae496d5609/slots/4c99c8bd-f117-4603-bba6-c8e2e9614799/2024/%s' % booking_date,
+            headers={"accept": "*/*",
+                    "accept-encoding": "gzip, deflate, br",
+                    "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
+                    "referer": "https://recreation.utoronto.ca/booking/af97abfd-f094-49c4-8a17-9330879ed6cc",
+                    "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-ch-ua-platform": "'windows'",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-origin",
+                    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+                    "x-requested-with": "XMLHttpRequest",
+                    "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
+                    })
+        updated_cookie = x.cookies.get_dict().get('.AspNet.ApplicationCookie')
+        print(updated_cookie)
+        application_cookie = application_cookie if updated_cookie is None else updated_cookie
+    except Exception as e:
+        print(f"Error updating cookie: {e}")
 
 # Used for pulling right before the hour, extract id, and book
 def extract_keyword(string, keyword):
@@ -91,24 +94,12 @@ def get_timeslot_string(string: str, hour: int, AMPM: str):
     # print(string)
     assert False
 
-
-def book_court():
-    data_apt_id1 = []
-    data_apt_id2 = []
-    data_apt_id3 = []
-    data_timeslot_id1 = []
-    data_timeslot_id2 = []
-    data_timeslot_id3 = []
-    time.sleep(0.05)
-    t0 = time.time()
-    while len(data_apt_id2) == 0 and time.time() - t0 < 180000:
-
-        # Dumb way to book all three courts -- but it worked anyways
-        request1 = requests.get('https://recreation.utoronto.ca/booking/%s/slots/%s/2024/%s' % (court1_id, court1_fid, booking_date),
+def get_court_request(court_id, court_fid):
+    return requests.get('https://recreation.utoronto.ca/booking/%s/slots/%s/2024/%s' % (court_id, court_fid, booking_date),
                          headers={"accept": "*/*",
                                   "accept-encoding": "gzip, deflate, br",
                                   "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                                  "referer": "https://recreation.utoronto.ca/booking/%s" % court1_id,
+                                  "referer": "https://recreation.utoronto.ca/booking/%s" % court_id,
                                   "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
                                   "sec-ch-ua-mobile": "?0",
                                   "sec-ch-ua-platform": "'windows'",
@@ -120,43 +111,62 @@ def book_court():
                                   "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
                                   })
 
-        request2 = requests.get(
-            'https://recreation.utoronto.ca/booking/%s/slots/%s/2024/%s' % (court2_id, court2_fid, booking_date),
-            headers={"accept": "*/*",
-                     "accept-encoding": "gzip, deflate, br",
-                     "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                     "referer": "https://recreation.utoronto.ca/booking/%s" % court2_id,
-                     "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                     "sec-ch-ua-mobile": "?0",
-                     "sec-ch-ua-platform": "'windows'",
-                     "sec-fetch-dest": "empty",
-                     "sec-fetch-mode": "cors",
-                     "sec-fetch-site": "same-origin",
-                     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-                     "x-requested-with": "XMLHttpRequest",
-                     "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
-                     })
+def book_court_request(court_id, data):
+    return requests.post('https://recreation.utoronto.ca/booking/reserve',
+                                        headers={"accept": "*/*",
+                                                "accept-encoding": "gzip, deflate, br",
+                                                "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
+                                                "referer": "https://recreation.utoronto.ca/booking/%s" % court_id,
+                                                "origin": "https://recreation.utoronto.ca",
+                                                "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                                                "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+                                                "sec-ch-ua-mobile": "?1",
+                                                "sec-ch-ua-platform": "'Android'",
+                                                "sec-fetch-dest": "empty",
+                                                "sec-fetch-mode": "cors",
+                                                "sec-fetch-site": "same-origin",
+                                                "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36",
+                                                "x-requested-with": "XMLHttpRequest",
+                                                "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
+                                                }, data=data)
 
-        request3 = requests.get(
-            'https://recreation.utoronto.ca/booking/%s/slots/%s/2024/%s' % (court3_id, court3_fid, booking_date),
-            headers={"accept": "*/*",
-                     "accept-encoding": "gzip, deflate, br",
-                     "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                     "referer": "https://recreation.utoronto.ca/booking/%s" % court3_id,
-                     "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                     "sec-ch-ua-mobile": "?0",
-                     "sec-ch-ua-platform": "'windows'",
-                     "sec-fetch-dest": "empty",
-                     "sec-fetch-mode": "cors",
-                     "sec-fetch-site": "same-origin",
-                     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-                     "x-requested-with": "XMLHttpRequest",
-                     "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
-                     })
+def construct_book_court_data(court_id, court_fid, apt_id, timeslot_id):
+    return {
+        "bId": court_id,  # booking/court-id
+        "fId": court_fid,  # No change
+        "aId": apt_id,
+        "tsId": timeslot_id,
+        "tsiId": "00000000-0000-0000-0000-000000000000",  # Sometimes m
+        "y": year,
+        "m": month,
+        "d": day,
+        "t": "",
+        "v": "0"
+    }
+
+def book_court():
+    data_apt_id1 = []
+    data_apt_id2 = []
+    data_apt_id3 = []
+    data_timeslot_id1 = []
+    data_timeslot_id2 = []
+    data_timeslot_id3 = []
+    t0 = time.time()
+    print("begin booking")
+    while (len(data_apt_id2) == 0 or len(data_apt_id1) == 0 or len(data_apt_id3) == 0) and time.time() - t0 < 180000:
+
+        # Dumb way to book all three courts -- but it worked anyways
+        request1 = get_court_request(court1_id, court1_fid)
+
+        request2 = get_court_request(court2_id, court2_fid)
+
+        request3 = get_court_request(court3_id, court3_fid)
 
         timeslot_string1 = get_timeslot_string(request1.text, hour=book_hour, AMPM=am_or_pm)
         data_apt_id1 = extract_keyword(timeslot_string1, "data-apt-id")
         data_timeslot_id1 = extract_keyword(timeslot_string1, "data-timeslot-id")
+
+        print(timeslot_string1)
 
         timeslot_string2 = get_timeslot_string(request2.text, hour=book_hour, AMPM=am_or_pm)
         data_apt_id2 = extract_keyword(timeslot_string2, "data-apt-id")
@@ -166,112 +176,39 @@ def book_court():
         data_apt_id3 = extract_keyword(timeslot_string3, "data-apt-id")
         data_timeslot_id3 = extract_keyword(timeslot_string3, "data-timeslot-id")
 
-        # data_timeslotinstance_id = extract_keyword(timeslot_string, "data-timeslotinstance-id")
-        if len(data_apt_id2) > 0:
-            data1 = {
-                "bId": court1_id,  # booking/court-id
-                "fId": court1_fid,  # No change
-                "aId": data_apt_id1[0],
-                "tsId": data_timeslot_id1[0],
-                "tsiId": "00000000-0000-0000-0000-000000000000",  # Sometimes m
-                "y": year,
-                "m": month,
-                "d": day,
-                "t": "",
-                "v": "0"}
+        try:
+            if len(data_apt_id2) > 0:
+                data2 = construct_book_court_data(court2_id, court2_fid, data_apt_id2[0], data_timeslot_id2[0])
+                
+                booking2 = book_court_request(court2_id, data2)
+                
+                print(f"Booking 2: {booking2.text}")
+        except Exception as e:
+            print(f"Error for court 2: {e}")
 
-            data2 = {
-                "bId": court2_id,  # booking/court-id
-                "fId": court2_fid,  # No change
-                "aId": data_apt_id2[0],
-                "tsId": data_timeslot_id2[0],
-                "tsiId": "00000000-0000-0000-0000-000000000000",  # Sometimes m
-                "y": year,
-                "m": month,
-                "d": day,
-                "t": "",
-                "v": "0"}
+        try:
+            if len(data_apt_id1) > 0:
+                data1 = construct_book_court_data(court1_id, court1_fid, data_apt_id1[0], data_timeslot_id1[0])
+                
+                booking1 = book_court_request(court1_id, data1)
+                
+                print(f"Booking 1: {booking1.text}")
 
-            data3 = {
-                "bId": court3_id,  # booking/court-id
-                "fId": court3_fid,  # No change
-                "aId": data_apt_id3[0],
-                "tsId": data_timeslot_id3[0],
-                "tsiId": "00000000-0000-0000-0000-000000000000",  # Sometimes m
-                "y": year,
-                "m": month,
-                "d": day,
-                "t": "",
-                "v": "0"}
+        except Exception as e:
+            print(f"Error for court 1: {e}")
 
-            booking2 = requests.post('https://recreation.utoronto.ca/booking/reserve',
-                                     headers={"accept": "*/*",
-                                              "accept-encoding": "gzip, deflate, br",
-                                              "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                                              "referer": "https://recreation.utoronto.ca/booking/%s" % court2_id,
-                                              "origin": "https://recreation.utoronto.ca",
-                                              "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                                              "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                                              "sec-ch-ua-mobile": "?1",
-                                              "sec-ch-ua-platform": "'Android'",
-                                              "sec-fetch-dest": "empty",
-                                              "sec-fetch-mode": "cors",
-                                              "sec-fetch-site": "same-origin",
-                                              "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36",
-                                              "x-requested-with": "XMLHttpRequest",
-                                              "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
-                                              }, data=data2)
+        try:
+            if len(data_apt_id3) > 0:
+                data3 = construct_book_court_data(court3_id, court3_fid, data_apt_id3[0], data_timeslot_id3[0])
+                    
+                booking3 = book_court_request(court3_id, data3)
+                print(f"Booking 3: {booking3.text}")
+        except Exception as e:
+            print(f"Error for court 3: {e}")
 
-
-            booking1 = requests.post('https://recreation.utoronto.ca/booking/reserve',
-                                    headers={"accept": "*/*",
-                                             "accept-encoding": "gzip, deflate, br",
-                                             "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                                             "referer": "https://recreation.utoronto.ca/booking/%s" % court1_id,
-                                             "origin": "https://recreation.utoronto.ca",
-                                             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                                             "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                                             "sec-ch-ua-mobile": "?1",
-                                             "sec-ch-ua-platform": "'Android'",
-                                             "sec-fetch-dest": "empty",
-                                             "sec-fetch-mode": "cors",
-                                             "sec-fetch-site": "same-origin",
-                                             "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36",
-                                             "x-requested-with": "XMLHttpRequest",
-                                             "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
-                                             }, data=data1)
-
-
-
-            booking3 = requests.post('https://recreation.utoronto.ca/booking/reserve',
-                                    headers={"accept": "*/*",
-                                             "accept-encoding": "gzip, deflate, br",
-                                             "accept-language": "en-GB,en;q=0.9,en-US;q=0.8,zh-CN;q=0.7,zh;q=0.6,zh-TW;q=0.5,und;q=0.4",
-                                             "referer": "https://recreation.utoronto.ca/booking/%s" % court3_id,
-                                             "origin": "https://recreation.utoronto.ca",
-                                             "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-                                             "sec-ch-ua": '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
-                                             "sec-ch-ua-mobile": "?1",
-                                             "sec-ch-ua-platform": "'Android'",
-                                             "sec-fetch-dest": "empty",
-                                             "sec-fetch-mode": "cors",
-                                             "sec-fetch-site": "same-origin",
-                                             "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Mobile Safari/537.36",
-                                             "x-requested-with": "XMLHttpRequest",
-                                             "cookie": ".AspNet.ApplicationCookie=%s" % application_cookie,
-                                             }, data=data3)
-            # print(data)
-            print("finished sending request")
-            print(datetime.now())
-            print(booking1.text)
-            print(booking2.text)
-            print(booking3.text)
-
-        else:
+        if len(data_apt_id2) == 0 and len(data_apt_id1) == 0 and len(data_apt_id3) == 0:
             print("no data yet.")
             print(datetime.now())
-            # print(timeslot_string)
-            # time.sleep(0.05) #This line can be removed if need to aggressively compete with other bots.
 
 
 def book_courts(book_hour, am_or_pm, court_id, fid):
---
+++ run.sh
@@ -0,0 +1,2 @@
+source ./.venv/Scripts/activate
+python cogs/CourtBookingFiles/BookCourt.py 7 11 [7] [PM] 1 <app_token>
