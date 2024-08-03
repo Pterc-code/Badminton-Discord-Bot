54dc8d380dd52d3edb8febde44f80427871be466
taikunzhang <taikun.zhang@mail.utoronto.ca>
2024-08-03 15:24:03 -0400

    adding comments

--- booking_bot.py
+++ booking_bot.py
@@ -4,12 +4,12 @@ import subprocess
 import os
 
 # change these values to the date and time you want to book
-username = ""   # username login for recreationurotono.ca
-password = ""   # password
-month = '8 '    # month of the year, for example august is 8
-day   = '06 '   # date of the month, for example 4th is 04 and 21 is 21
-time  = '[6] '  # time of the day
-am    = '[PM] ' # [PM] or [AM]
+username = ""  # username login for recreation.utoronto.ca
+password = ""  # password logic for recreation.utoronto.ca
+month = '8'    # month of the year, for example august is 8
+day   = '06'   # date of the month, for example 4th is 04 and 21 is 21
+time  = '[6]'  # time of the day, for example 6pm is '[6] '
+am    = '[PM]' # [PM] or [AM]
 
 with requests.Session() as s:
   recreations_page = s.get("https://recreation.utoronto.ca/")
@@ -19,5 +19,5 @@ with requests.Session() as s:
 token = login.headers['Set-Cookie'].split(';')[0].split('.AspNet.ApplicationCookie=')[1] 
 cwd = os.getcwd()
 os.chdir(cwd + "/cogs/CourtBookingFiles/")
-command = "python BookCourt.py " +  month + day + time + am + " 1 " + token
+command = "python BookCourt.py " +  month + ' ' +  day + ' ' + time + ' ' + am + " 1 " + token
 subprocess.run(command, shell=True) 
